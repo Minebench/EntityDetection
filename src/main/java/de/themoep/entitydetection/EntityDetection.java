383b391182e5044f8febc310eee8e683ba545ba4
Max Lee <Phoenix616@users.noreply.github.com>
2016-03-22 03:57:22 +0100

    Add a caption to the entity type hover list

--- src/main/java/de/themoep/entitydetection/EntityDetection.java
+++ src/main/java/de/themoep/entitydetection/EntityDetection.java
@@ -109,7 +109,7 @@ public class EntityDetection extends JavaPlugin {
 
         int start = page * 10;
         if(serverIsSpigot && sender instanceof Player) {
-            String searchedTypes = "";
+            String searchedTypes = ChatColor.YELLOW + "Entity Types:\n";
             Iterator<EntityType> typeIter = result.getSearchedEntities().iterator();
             while(typeIter.hasNext()) {
                 searchedTypes += ChatColor.DARK_PURPLE + Utils.enumToHumanName(typeIter.next());
